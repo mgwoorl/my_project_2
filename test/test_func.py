d0998d64b984dfed971e4efd69f1724e9ab5e053
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-04-16 20:32:40 +0800

    Added test for getting length of vector

--- test/test_func.py
+++ test/test_func.py
@@ -1,1 +1,9 @@
+import pytest
+from code.func import Vector
 
+class TestVector:
+    def test_length(self):
+        v = Vector(3, 7)
+        out = 7.615773105863909
+        res = v.length()
+        assert res == out
