5adae05c23fe0876ae3bf94a37b7b814c8fe5765
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-04-16 11:01:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup
 
+setup(
+   name='vector_operations',
+   version='1.0',
+   description='For vector operations',
+   license='MIT',
+   author='Oksana Galiulina',
+   author_email='oksana17.06@bk.ru',
+   url='https://github.com/mgwoorl/my_project2',
+   packages=['code'], 
+   install_requires=[], # it is empty since we use standard python library
+   extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+   },
+   python_requires='>=3',
+)
